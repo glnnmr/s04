191d18d0a1a64d4d9d9c25a74b12059877b25e04
glnnmr <93329116+glnnmr@users.noreply.github.com>
2022-10-22 00:47:34 +0800

    Add activity code.

---
+++ s04/aggregation.js
@@ -0,0 +1,222 @@
+//employee
+
+{
+    "_id" : ObjectId("63299872105da568c2cf753d"),
+    "id" : "200009",
+    "name" : "Rocky",
+    "age" : 29,
+    "jobRole" : "Manager",
+    "salary" : "70000"
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("63299895105da568c2cf7544"),
+    "id" : "200010",
+    "name" : "Ferg",
+    "age" : 28,
+    "jobRole" : "Supervisor",
+    "salary" : "65000"
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("632998b7105da568c2cf7553"),
+    "id" : "200011",
+    "name" : "Yam",
+    "age" : 25,
+    "jobRole" : "Cashier",
+    "salary" : "15000"
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("6329991b105da568c2cf7586"),
+    "id" : "200012",
+    "name" : "Mob",
+    "age" : 27,
+    "jobRole" : "Security Guard",
+    "salary" : "11000"
+}
+
+//inventory
+
+/* 1 */
+{
+    "_id" : ObjectId("63299e60105da568c2cf778b"),
+    "id" : "LS0000123",
+    "name" : "Chocolate Bar - 100g",
+    "price" : "5.23",
+    "quantity" : "25000",
+    "category" : "chocolates, sweets"
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("63299ea5105da568c2cf77ae"),
+    "id" : "LS0003123",
+    "name" : "Milk Non-Fat - 1lt",
+    "price" : "3",
+    "quantity" : "1000",
+    "category" : "dairy, healthy"
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("63299ee3105da568c2cf77ce"),
+    "id" : "LS0004566",
+    "name" : "Eggs - 12 pack",
+    "price" : "6",
+    "quantity" : "5000",
+    "category" : "poultry, generic"
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("63299f41105da568c2cf77f9"),
+    "id" : "LS0008542",
+    "name" : "Whole Chicken",
+    "price" : "12.59",
+    "quantity" : "1250",
+    "category" : "poultry, meat"
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("63299f7b105da568c2cf7814"),
+    "id" : "LS0008642",
+    "name" : "Carrots (Packed - 250g",
+    "price" : "3.50",
+    "quantity" : "3000",
+    "category" : "vegetables, healthy, organic"
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("63299fa5105da568c2cf7827"),
+    "id" : "LS0009846",
+    "name" : "Beans (Packed - 250g",
+    "price" : "6.75",
+    "quantity" : "6000",
+    "category" : "vegetables, healthy, organic"
+}
+
+/* 7 */
+{
+    "_id" : ObjectId("63299fce105da568c2cf7838"),
+    "id" : "LS0009100",
+    "name" : "Bell Pepper (Packed - 250g",
+    "price" : "4.95",
+    "quantity" : "12000",
+    "category" : "vegetables, healthy, organic"
+}
+
+/* 8 */
+{
+    "_id" : ObjectId("6329a007105da568c2cf7851"),
+    "id" : "LS0002688",
+    "name" : "ZZ Butter - 500g",
+    "price" : "25",
+    "quantity" : "500",
+    "category" : "dairy, healthy, premium"
+}
+
+//payments
+
+/* 1 */
+{
+    "_id" : ObjectId("6329a0a1105da568c2cf789a"),
+    "id" : "BL2021005",
+    "Gross Amount" : "105.65",
+    "Discounts" : "10",
+    "Net Amount" : "95.65",
+    "Date/Time" : "ISODATE2021-01-01T16:00:00Z"
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("6329a0cf105da568c2cf78af"),
+    "id" : "BL2021006",
+    "Gross Amount" : "45.25",
+    "Discounts" : "0",
+    "Net Amount" : "45.25",
+    "Date/Time" : "ISODATE2021-01-01T16:15:55Z"
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("6329a0fe105da568c2cf78c4"),
+    "id" : "BL2021007",
+    "Gross Amount" : "153.33",
+    "Discounts" : "20.33",
+    "Net Amount" : "133",
+    "Date/Time" : "ISODATE2021-01-01T16:31:08Z"
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("6329a14d105da568c2cf78ef"),
+    "id" : "BL2021008",
+    "Gross Amount" : "21",
+    "Discounts" : "0",
+    "Net Amount" : "21",
+    "Date/Time" : "ISODATE2021-01-01T20:25:52Z"
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("6329a195105da568c2cf790c"),
+    "id" : "BL2021009",
+    "Gross Amount" : "89.72",
+    "Discounts" : "0.72",
+    "Net Amount" : "89",
+    "Date/Time" : "ISODATE2021-01-01T08:45:12Z"
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("6329a1c6105da568c2cf7925"),
+    "id" : "BL2021010",
+    "Gross Amount" : "33.5",
+    "Discounts" : "20.5",
+    "Net Amount" : "13",
+    "Date/Time" : "ISODATE2021-01-01T11:02:35Z"
+}
+
+//promo
+
+/* 1 */
+{
+    "_id" : ObjectId("6329a218105da568c2cf7946"),
+    "id" : "PROMO01",
+    "Name" : "Sales Promo",
+    "Period" : "7",
+    "Daily Sales" : "20,50,12,30,45,15,60"
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("6329a234105da568c2cf7957"),
+    "id" : "PROMO02",
+    "Name" : "Milk Promo",
+    "Period" : "2",
+    "Daily Sales" : "120,200"
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("6329a2d5105da568c2cf7998"),
+    "id" : "PROMO03",
+    "Name" : "Meat Promo",
+    "Period" : "3",
+    "Daily Sales" : "101,205"
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("6329a35c105da568c2cf79e4"),
+    "id" : "PROMO04",
+    "Name" : "New Year Promo",
+    "Period" : "7",
+    "Daily Sales" : "68,88,105,188,74,278,350"
+}
